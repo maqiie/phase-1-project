c5d2665c27aba62955c08a0cf87d03d15577a443
maqiie <99504010+maqiie@users.noreply.github.com>
2022-12-15 13:28:01 +0300

    fetched the api (https://api.frankfurter.app/currencies)

--- scripts.js
+++ scripts.js
@@ -2,3 +2,8 @@ const select = document.querySelectorAll(".currency");
 const btn = document.getElementById("btn");
 const num = document.getElementById("num");
 const ans = document.getElementById("ans");
+fetch("https://api.frankfurter.app/currencies")
+  .then((data) => data.json())
+  .then((data) => {
+    display(data);
+  });
