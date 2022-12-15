3af0c8ce1f25a2e74cf30c33dbe82dfb3d8a5c67
maqiie <99504010+maqiie@users.noreply.github.com>
2022-12-15 13:26:53 +0300

    started working on js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+const select = document.querySelectorAll(".currency");
+const btn = document.getElementById("btn");
+const num = document.getElementById("num");
+const ans = document.getElementById("ans");
