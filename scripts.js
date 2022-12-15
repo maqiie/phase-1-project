1d4a39af574a1e0f53a8b03aee2a5615eb2efcaa
maqiie <99504010+maqiie@users.noreply.github.com>
2022-12-15 13:34:37 +0300

    added event listeners

--- scripts.js
+++ scripts.js
@@ -7,3 +7,23 @@ fetch("https://api.frankfurter.app/currencies")
   .then((data) => {
     display(data);
   });
+//displays the data from the api
+  function display(data) {
+    const entries = Object.entries(data);
+    for (var i = 0; i < entries.length; i++) {
+      select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+      select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+    }
+    //added the event listeners for click
+  }
+  btn.addEventListener("click", () => {
+    let currency1 = select[0].value;
+    let currency2 = select[1].value;
+    let value = num.value;
+  
+    if (currency1 != currency2) {
+      convert(currency1, currency2, value);
+    } else {
+      alert("Choose Different Currencies !!!");
+    }
+  });
